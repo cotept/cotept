f425c049ddaa932bcc66ac8d28a363522448bac9
BounbleJ <dogmnil2007@naver.com>
2025-05-28 00:13:22 +0900

    feat: gitignore에 vscode 추가, 앱라우팅 테스트

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import React from "react"
 
@@ -7,20 +7,14 @@ import "./app.css"
 import Provider from "~/app/providers/Provider"
 import ReactQueryProvider from "~/app/providers/ReactQueryProvider"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <ReactQueryProvider>
-          <Provider>
-            {children}
-          </Provider>
+          <Provider>{children}</Provider>
         </ReactQueryProvider>
       </body>
     </html>
   )
-} 
+}
---
+++ apps/web/app/mentoring/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Mentoring from "~/page/mentoring"
+
+export default function Page() {
+  return (
+    <div>
+      <Mentoring />
+      page
+    </div>
+  )
+}

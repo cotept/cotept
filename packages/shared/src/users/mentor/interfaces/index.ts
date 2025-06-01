5837ae3c98bd0330d810f96ccf1f5c792c9b3311
HyunSJ <dudtod1596@gmail.com>
2025-06-01 20:45:37 +0900

    Remove: 사용하지 않는 공유 파일 삭제

--- packages/shared/src/users/mentor/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./mentor-profile.interface"
-export * from "./mentor-schedule.interface"
-export * from "./mentor.interface"
-export * from "./metor-social-account.interface"
--- packages/shared/src/users/mentor/interfaces/metor-social-account.interface.ts
+++
@@ -1,6 +0,0 @@
-//metor-social-account.interface.ts
-import { ISocialAccount } from "../../../auth"
-
-export interface IMentorSocialAccount extends ISocialAccount {
-  mentorId: string
-}

9a6ac40138cbf4f9e29f1c0d65b0eb3991117da5
BounbleJ <dogmnil2007@naver.com>
2025-06-03 01:29:34 +0900

    feat: msw 세팅

--- apps/web/mocks/handlers.ts
+++ apps/web/mocks/handlers.ts
@@ -1,11 +1,21 @@
 import { http, HttpResponse } from 'msw'
+import { mentor } from "./data"
  
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
 export const handlers = [
-  http.get('https://api.example.com/user', () => {
-    return HttpResponse.json({
-      id: 'abc-123',
-      firstName: 'John',
-      lastName: 'Maverick',
-    })
+  // 사용자 API 핸들러
+  http.get(`${baseUrl}/api/users`, () => {
+    return HttpResponse.json(mentor)
+  }),
+
+  http.get(`${baseUrl}/api/users/:id`, ({ params }) => {
+    const user = mentor.find((mentor) => mentor.id === params.id)
+
+    if (!user) {
+      return new HttpResponse(null, { status: 404 })
+    }
+
+    return HttpResponse.json(user)
   }),
 ]

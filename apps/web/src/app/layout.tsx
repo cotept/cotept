061d8b39d75cd5d5f2fec366eeed10ff0016c0f3
goldmayo <dudtod1596@gmail.com>
2026-01-06 01:43:10 +0900

    feat(web/font): Pretendard 폰트 추가

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,12 +2,21 @@ import { Toaster } from "@repo/shared/components/sonner"
 
 import "@repo/shared/globals.css"
 
+import localFont from "next/font/local"
+
 import Providers from "@/app/_provider/Providers"
+
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+})
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
+    <html lang="ko" suppressHydrationWarning>
+      <body className={pretendard.variable}>
         <Providers>{children}</Providers>
         <Toaster />
         <div id="overlay-root"></div>

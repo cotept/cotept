0c798beba284f323128b7531ff92a96c58ce389d
goldmayo <dudtod1596@gmail.com>
2026-02-01 02:21:57 +0900

    feat(web): 외부 이미지 도메인 추가

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.cotept.com",
+        port: "", // 포트는 비워두면 기본 443(https)이 적용
+        pathname: "/public/**",
+      },
+    ],
+  },
 }
 
 export default nextConfig
